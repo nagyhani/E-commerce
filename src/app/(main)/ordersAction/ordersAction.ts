46a52be7b40ec12f5c7aef50f210b20be5c361f4
Nagy Hani <nagyhani337@gmail.com>
2025-09-16 05:46:36 +0300

    khla

--- src/app/(main)/ordersAction/ordersAction.ts
+++ src/app/(main)/ordersAction/ordersAction.ts
@@ -60,9 +60,10 @@ export async function cashPaymentSession(id : string , shippingData : {details :
 
 
 export async function ordersReview(){
+    const TokenSession = (process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token")
      const token = await getUserToken()
      const cookiesData = await cookies()
-     const encryptToken =  cookiesData.get('next-auth.session-token')?.value
+     const encryptToken =  cookiesData.get(TokenSession)?.value
  const decryptToken = await  decode({token : encryptToken , secret: process.env.AUTH_SECRET!})
 
 
