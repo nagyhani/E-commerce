46a0bc0474b95dda207c67592e5db2a001bb42c1
Nagy Hani <nagyhani337@gmail.com>
2025-09-16 04:28:29 +0300

    Change token name

--- src/getUserToken.ts
+++ src/getUserToken.ts
@@ -3,9 +3,11 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
+
+const TokenSession = (process.env.NODE_ENV === "production" ? "__Secure-next-auth.session-token" : "next-auth.session-token")
 export async function getUserToken(){
   const cookiesData = await cookies()
- const encryptToken =  cookiesData.get('next-auth.session-token')?.value
+ const encryptToken =  cookiesData.get(TokenSession)?.value
 
  const decryptToken = await  decode({token : encryptToken , secret: process.env.AUTH_SECRET!})
 
